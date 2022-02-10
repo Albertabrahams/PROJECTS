35350ca0280fda2a08be837b7c0d116cd876aa9d
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-02-10 21:46:12 +0300

    added

---
+++ age calculator/script.js
@@ -0,0 +1,15 @@
+const years = document.getElementById("years");
+const months = document.getElementById("months");
+const days = document.getElementById("days");
+const hours = document.querySelector("#hours");
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
+
+const loading = document.querySelector(".loading");
+window.addEventListener("load", ()=>{
+    loading.style.display = "block";
+    //miliseconds in seTimeout
+    setTimeout(()=>{
+        loading.style.display = "none";
+    },2000)
+});
