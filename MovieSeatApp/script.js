0ca09622a5573fcb5f65307dbdf72cf8c2f2a701
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-03-13 13:15:32 +0300

    project done

--- MovieSeatApp/script.js
+++ MovieSeatApp/script.js
@@ -8,11 +8,17 @@ const movieSelectBox = document.getElementById("movie");
 
 let currentTicketPrice = localStorage.getItem("selectedMoviePrice") ? localStorage.getItem("selectedMoviePrice") : movieSelectBox.options[movieSelectBox.selectedIndex].value;
 
-let currentMovieIndex = localStorage.getItem("selectedMovieIndex") ? localStorage.getItem("selectedMovieIndex") : movieSelectBox.selectedIndex
+let currentMovieIndex = localStorage.getItem("selectedMovieIndex") ? localStorage.getItem("selectedMovieIndex") : movieSelectBox.selectedIndex;
+
+window.onload = () => {
+    displaySeats();
+    updateMovieInfo();
+}
 
 movieSelectBox.addEventListener("change", (e)=>{
     let ticketPrice = e.target.value;
     let movieIndex = e.target.selectedIndex;
+    updateMovieInfo();
     setMovieDataToLocalStorage(ticketPrice,movieIndex);
 });
 
@@ -34,10 +40,25 @@ container.addEventListener("click", (e)=>{
 
 const updateMovieInfo = () =>{
     let selectedSeats = document.querySelectorAll(".row .seat.selected");
-    console.log(selectedSeats);
-    console.log([...selectedSeats]);
+
     let selectedSeatsIndexArray = [...selectedSeats].map(seat=>[...allSeats].indexOf(seat));
     localStorage.setItem("selectedSeats", JSON.stringify(selectedSeatsIndexArray));
     console.log(selectedSeatsIndexArray);
+
+    count.innerText = selectedSeatsIndexArray.length;
+    total.innerText = selectedSeatsIndexArray.length * movieSelectBox.value;
+    film.innerText = movieSelectBox.options[movieSelectBox.selectedIndex].innerText.split("(")[0];
 }
 
+const displaySeats = () => {
+    movieSelectBox.selectedIndex = currentMovieIndex;
+    let selectedSeatsFromStorage = JSON.parse(localStorage.getItem("selectedSeats"));
+    console.log(selectedSeatsFromStorage);
+    if(selectedSeatsFromStorage !== null && selectedSeatsFromStorage.length > 0){
+        allSeats.forEach((seat,index)=>{
+            if(selectedSeatsFromStorage.indexOf(index)>-1){
+            seat.classList.add("selected");
+        }
+    })};
+
+}
