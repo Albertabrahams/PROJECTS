dcdc2feca5b598f5f8360494b7382c196acbd5a9
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-03-13 10:23:27 +0300

    added

--- MovieSeatApp/script.js
+++ MovieSeatApp/script.js
@@ -6,6 +6,10 @@ const film = document.getElementById("film");
 const total = document.getElementById("total");
 const movieSelectBox = document.getElementById("movie");
 
+let currentTicketPrice = localStorage.getItem("selectedMoviePrice") ? localStorage.getItem("selectedMoviePrice") : movieSelectBox.options[movieSelectBox.selectedIndex].value;
+
+let currentMovieIndex = localStorage.getItem("selectedMovieIndex") ? localStorage.getItem("selectedMovieIndex") : movieSelectBox.selectedIndex
+
 movieSelectBox.addEventListener("change", (e)=>{
     let ticketPrice = e.target.value;
     let movieIndex = e.target.selectedIndex;
@@ -25,4 +29,15 @@ container.addEventListener("click", (e)=>{
     // if(e.target.classList.contains("occupied")){
     //     alert("Please choose an available seat")
     // }
-})
+    updateMovieInfo();
+});
+
+const updateMovieInfo = () =>{
+    let selectedSeats = document.querySelectorAll(".row .seat.selected");
+    console.log(selectedSeats);
+    console.log([...selectedSeats]);
+    let selectedSeatsIndexArray = [...selectedSeats].map(seat=>[...allSeats].indexOf(seat));
+    localStorage.setItem("selectedSeats", JSON.stringify(selectedSeatsIndexArray));
+    console.log(selectedSeatsIndexArray);
+}
+
